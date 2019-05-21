c1383205cf8e4a22f4349acf02e29e49ba7637e6
sidibemohamadou <49401334+sidibemohamadou@users.noreply.github.com>
2019-05-21 09:47:12 +0000

    ranger aussi

---
+++ menu_burger/Html_Css/js/style.js
@@ -0,0 +1,10 @@
+function openslideMenu(){
+    document.getElementById('menu').style.width ='250px';
+    document.getElementById('content').style.marginLeft ='250px';
+
+}
+function closeslideMenu(){
+    document.getElementById('menu').style.width ='0';
+    document.getElementById('content').style.marginLeft ='0';
+
+}
